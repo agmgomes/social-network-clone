09f8470df9a0c310b0e5abf6f1db1039b0d39d5a
Andre <agmgomes94@gmail.com>
2019-12-17 14:50:01 +0000

    added new routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 const users = require('./users');
+const posts = require('./posts');
+const comments = require('./comments');
+const auth = require('./auth');
 
 router.use('/users', users);
+router.use('/posts', posts);
+router.use('/comments', comments);
+router.use('/auth', auth);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,27 +7,55 @@ const userController = require('../controllers/userController');
  * desc Get all users
  * access Public
  */
-router.get('/', userController.getAll);
+router.get('/', userController.getAllUsers);
 
 /**
  * route GET /users/:id
  * desc Get user by Id
  * access Public
  */
-router.get('/:id', userController.getByID);
+router.get('/:id', userController.getUserByID);
 
 /**
- * route POST /users/register
+ * route GET /users/:id/posts
+ * desc Get all user's posts
+ * access Public
+ */
+router.get('/:id/posts', userController.getAllUserPosts);
+
+/**
+ * route GET /users/:id/posts/:post_id
+ * desc Get a single post from a user
+ * access Public
+ */
+router.get('/:id/posts/:post_id', userController.getUserPostByID);
+
+/**
+ * route GET /users/:id/comments/
+ * desc Get all user's comments
+ * access Public
+ */
+router.get('/:id/comments', userController.getAllUserComents);
+
+/**
+ * route GET /users/:id/comments/:comment_id
+ * desc Get a single comment from a user
+ * access Public
+ */
+router.get('/:id/comments/:comment_id', userController.getUserCommentByID);
+
+/**
+ * route POST /users/
  * desc Register a new user
  * access Public
  */
-router.post('/register', userController.register);
+router.post('/', userController.registerUser);
 
 /**
- * route DELETE /users/delete/:id
+ * route DELETE /users/:id
  * desc Delete a user
  * access Public
  */
-router.delete('/delete/:id', userController.delete);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
