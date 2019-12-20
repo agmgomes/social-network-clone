4188b88db90a5bad459933a689a2d94c72bc19ea
Andre <agmgomes94@gmail.com>
2019-12-20 20:21:22 +0000

    authValidator updated

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -1,16 +1,17 @@
-const { encryptionHelper } = require('../services');
+const { encryptionHelper, responseSender } = require('../services');
 
 module.exports = {
   validateToken: async (req, res, next) => {
     const { token } = req;
 
-    if (!token) return res.status(400).json({ msg: 'no token provided' });
+    if (!token) return responseSender.badRequest(res, 'No token provided');
 
     let decodedToken = encryptionHelper.verifyToken(token);
 
-    if (!decodedToken) return res.status(401).json({ msg: 'invalid token' });
+    if (!decodedToken) return responseSender.forbidden(res, 'Invalid token');
 
     res.locals = decodedToken;
+
     next();
   }
 };
